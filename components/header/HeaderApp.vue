35512460f1836f0b77b403cf3f7806b0fdfb4464
QuyenNguyen230698 <166916954+QuyenNguyen230698@users.noreply.github.com>
2025-12-07 16:18:29 +0700

    update header

--- components/header/HeaderApp.vue
+++ components/header/HeaderApp.vue
@@ -184,87 +184,11 @@
           class="h-full flex navbar-end lg:flex-row items-center justify-end"
         >
           <div class="flex gap-3 items-center lg:hidden">
-            <div class="relative">
-              <!-- Nút click để mở dropdown -->
-              <div @click="toggleDropdown" class="cursor-pointer">
-                <p
-                  class="text-sm px-4 py-2 uppercase whitespace-nowrap flex gap-1 items-center justify-center text-white hover:text-pyramid-gold bg-pyramid-gold hover:bg-white cursor-pointer transition-all duration-300"
-                  :class="isDropdownOpen ? 'rounded-t-3xl' : 'rounded-3xl'"
-                >
-                  <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-6">
+            <label for="menu-drawer-mobile" class="rounded-full w-fit bg-transparent border border-pyramid-gold p-2 flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 text-pyramid-gold">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                   </svg>
-                  </span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1"
-                    stroke="currentColor"
-                    :class="[
-                      'size-4 transition-transform duration-300',
-                      { 'rotate-180': isDropdownOpen },
-                    ]"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="m19.5 8.25-7.5 7.5-7.5-7.5"
-                    />
-                  </svg>
-                </p>
-              </div>
-
-              <!-- Dropdown menu -->
-              <ul
-                v-if="isDropdownOpen"
-                v-click-away="closeDropdown"
-                class="absolute right-0 top-9 bg-white z-50 text-left fade-down w-52 rounded-b-3xl rounded-tl-3xl shadow-lg"
-              >
-                <li class="text-sm pt-2 px-4">
-                  <NuxtLink
-                    to="/san-pham/biet-thu-don-lap"
-                    external
-                    :class="
-                      isActiveRoute('/san-pham/biet-thu-don-lap')
-                        ? 'font-montserrat-medium text-pyramid-gold'
-                        : 'font-montserrat-medium hover:text-pyramid-gold'
-                    "
-                    @click="closeDropdown"
-                  >
-                    BIỆT THỰ ĐƠN LẬP
-                  </NuxtLink>
-                </li>
-                <li class="hover:text-pyramid-gold text-sm py-2 px-4">
-                  <NuxtLink
-                    to="/san-pham/biet-thu-song-lap"
-                    external
-                    :class="
-                      isActiveRoute('/san-pham/biet-thu-song-lap')
-                        ? 'font-montserrat-medium text-pyramid-gold'
-                        : 'font-montserrat-medium hover:text-pyramid-gold'
-                    "
-                    @click="closeDropdown"
-                  >
-                    BIỆT THỰ SONG LẬP
-                  </NuxtLink>
-                </li>
-                <li class="hover:text-pyramid-gold text-sm px-4 pb-2">
-                  <NuxtLink
-                    to="/san-pham/nha-pho-thuong-mai"
-                    external
-                    :class="
-                      isActiveRoute('/san-pham/nha-pho-thuong-mai')
-                        ? 'font-montserrat-medium text-pyramid-gold'
-                        : 'font-montserrat-medium hover:text-pyramid-gold'
-                    "
-                    @click="closeDropdown"
-                  >
-                    NHÀ PHỐ THƯƠNG MẠI
-                  </NuxtLink>
-                </li>
-              </ul>
-            </div>
+            </label>
             <div @click="openSearchModal"
               class="py-2 flex-grow flex items-center justify-center rounded-full bg-transparent border border-pyramid-gold p-2"
             >
@@ -299,7 +223,7 @@
           <div class="navbar-start flex items-center gap-6">
             <!-- Nút menu (mở drawer desktop) -->
             <label
-              for="my-drawer2"
+              for="menu-drawer-desktop"
               class="cursor-pointer px-4 py-1 rounded-full border border-pyramid-gold flex items-center justify-center gap-2 text-white hover:text-pyramid-gold transition-all duration-300"
             >
               <svg
@@ -461,7 +385,7 @@
       <!-- ==== DRAWER DESKTOP -->
       <div class="drawer">
         <input
-          id="my-drawer2"
+          id="menu-drawer-desktop"
           type="checkbox"
           class="drawer-toggle"
           v-model="isDrawerOpen2"
@@ -469,7 +393,7 @@
         <div class="drawer-content"></div>
 
         <div class="drawer-side z-50">
-          <label for="my-drawer2" class="drawer-overlay"></label>
+          <label for="menu-drawer-desktop" class="drawer-overlay"></label>
           <div
             class="bg-custom-green w-full lg:w-1/3 min-h-screen shadow-2xl overflow-y-auto"
           >
@@ -480,7 +404,7 @@
                 <div class="col-start-2 flex items-center gap-6">
                   <!-- Nút menu (mở drawer desktop) -->
                   <label
-                    for="my-drawer2"
+                    for="menu-drawer-desktop"
                     class="cursor-pointer px-4 py-1 rounded-full border border-pyramid-gold flex items-center justify-center gap-2 text-white hover:text-pyramid-gold transition-all duration-300"
                   >
                     <svg
@@ -597,6 +521,95 @@
           </div>
         </div>
       </div>
+
+      <!-- Drawer Products Mobile -->
+      <div class="drawer drawer-end">
+        <input
+          id="menu-drawer-mobile"
+          type="checkbox"
+          class="drawer-toggle"
+          v-model="isDrawerOpenProductMobile"
+        />
+        <div class="drawer-content"></div>
+
+        <div class="drawer-side z-50">
+          <label for="menu-drawer-mobile" class="drawer-overlay"></label>
+          <div
+            class="bg-custom-green w-full lg:w-1/3 min-h-screen shadow-2xl overflow-y-auto"
+          >
+            <ul
+              class="bg-custom-green text-base w-full h-full overflow-y-scroll px-4"
+            >
+                <li
+                  class="flex flex-row justify-between items-center w-full py-4"
+                >
+                  <NuxtLink aria-label="logo" external to="/" @click="closeDrawerMobile">
+                    <NuxtImg
+                      quality="75"
+                      loading="eager"
+                      src="/Logo.svg"
+                      aria-label="logo"
+                      alt="logo"
+                      class="min-w-full h-18 object-contain"
+                    />
+                  </NuxtLink>
+                  <label
+                    for="menu-drawer-mobile"
+                    aria-label="close sidebar"
+                    class="drawer-overlay"
+                  >
+                    <span class="p-2 rounded-full text-white cursor-pointer">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="2"
+                        stroke="currentColor"
+                        class="size-8"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M6 18 18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </span>
+                  </label>
+                </li>
+                  <li class="text-nowrap w-full pb-4">
+                    <NuxtLink
+                      to="/san-pham/biet-thu-don-lap"
+                      external
+                      @click="closeDrawerMobile"
+                      class="font-montserrat-medium col-start-2 text-white hover:text-pyramid-gold cursor-pointer transition-all duration-300"
+                    >
+                      <p class="text-2xl">Biệt Thự Đơn Lập</p>
+                    </NuxtLink>
+                  </li>
+                  <li class="text-nowrap w-full pb-4">
+                    <NuxtLink
+                      to="/san-pham/biet-thu-song-lap"
+                      external
+                      @click="closeDrawerMobile"
+                      class="font-montserrat-medium col-start-2 text-white hover:text-pyramid-gold cursor-pointer transition-all duration-300"
+                    >
+                      <p class="text-2xl">Biệt Thự Song Lập</p>
+                    </NuxtLink>
+                  </li>
+                  <li class="text-nowrap w-full pb-4">
+                    <NuxtLink
+                      to="/san-pham/nha-pho-thuong-mai"
+                      external
+                      @click="closeDrawerMobile"
+                      class="font-montserrat-medium col-start-2 text-white hover:text-pyramid-gold cursor-pointer transition-all duration-300"
+                    >
+                      <p class="text-2xl">Nhà Phố Thương Mại</p>
+                    </NuxtLink>
+                  </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </header>
   </div>
 </template>
@@ -623,6 +636,7 @@ const isDropdownOpen = ref(false);
 const isCollapseOpen = ref(false);
 const isDrawerOpen = ref(false);
 const isDrawerOpen2 = ref(false);
+const isDrawerOpenProductMobile = ref(false);
 let lastScrollY = 0;
 
 const toggleDropdown = () => {
@@ -646,6 +660,10 @@ const closeDrawer = () => {
 
 const closeDrawer2 = () => {
   isDrawerOpen2.value = false;
+};
+
+const closeDrawerMobile = () => {
+  isDrawerOpenProductMobile.value = false;
 };
 
 const closeCollapse = () => {
